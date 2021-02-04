cfe2ac3cb6308ef0c5eaa8b8d0da239ebc783b78
Daniel Villalobos <djvr91@gmail.com>
2021-02-04 17:41:00 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Daniel!'));
+app.get('/', (req, res) => res.send('Hola Prueba de Aprobación Manual!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
